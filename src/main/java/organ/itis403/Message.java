ea6d2e1c4288c0c67b27ab78a1a8f8f7528490da
Azat <azatkep118@gmail.com>
2024-12-04 13:31:52 +0300

    Class Message body created

---
+++ src/main/java/organ/itis403/Message.java
@@ -0,0 +1,50 @@
+package organ.itis403;
+
+public class Message {
+    private String userSend;
+    private String userGet;
+    private String text;
+    private String date;
+
+    public Message() {}
+
+    public Message(String userSend, String userGet, String text, String date) {
+        this.date = date;
+        this.text = text;
+        this.userGet = userGet;
+        this.userSend = userSend;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getUserGet() {
+        return userGet;
+    }
+
+    public String getUserSend() {
+        return userSend;
+    }
+
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setUserGet(String userGet) {
+        this.userGet = userGet;
+    }
+
+    public void setUserSend(String userSend) {
+        this.userSend = userSend;
+    }
+}
