f598861dde2b70d4755a870c0a9a6be56a5c8ac6
Azat <azatkep118@gmail.com>
2024-12-07 12:34:03 +0300

    Update message and user service class

--- src/main/java/service/MessageService.java
+++ src/main/java/service/MessageService.java
@@ -5,13 +5,12 @@ import organ.itis403.User;
 
 public class MessageService {
 
-    public String[] create4Message(User user1, User user2) {
-        String messages[] = new String[4];
+    public Message[] create4Message(User user1, User user2) {
+        Message messages[] = new Message[4];
         messages[0] = new Message(user1, user2, "hi","12.12.2012");
         messages[1] = new Message(user2, user1, "hi", "13.12.2012");
         messages[2] = new Message(user1, user2, "how are u?","13.12.2012");
         messages[3] = new Message(user2, user1,"im fine", "14.12.2012");
         return messages;
     }
-
 }
